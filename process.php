31276a4de6673e3b252f5458683795a0214fa573
ryanromanov <ryanromanosky@icloud.com>
2014-06-04 11:41:57 -0400

    Still need to fix offset mobile bug, hello php bug

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php
-header( "Refresh:3; url=http://localhost/EmBoxSorter/addSamples.html", true, 303);
-	$con=mysqli_connect("localhost","root","passw0rd","cbiBoxSorter");
+header( "Refresh:3; url=http://www.cbi.pitt.edu/EMBoxSorter/addSamples.html", true, 303);
+	$con=mysqli_connect("localhost","root","","cbiTest");
 	//Check Connection
 	if (mysqli_connect_errno($con)) {
 		echo mysqli_connect_error() . "Connection Failed! It... came... from... behind!";
@@ -10,15 +10,15 @@ header( "Refresh:3; url=http://localhost/EmBoxSorter/addSamples.html", true, 303
 	$box = mysqli_real_escape_string($con, $_POST['Box']);
 	$roh = mysqli_real_escape_string($con, $_POST['Row']);
 	$level = mysqli_real_escape_string($con, $_POST['Level']);
-		$dupes = "SELECT * FROM Boxes WHERE Number='$number'";
+		$dupes = "SELECT * FROM EMSampleBoxSorter WHERE Number='$number'";
 			$result = mysqli_query($con, $dupes);
 			if (mysqli_num_rows($result) > 0) {
-				die("Error: There's your\n" . $number . "\nand there's my\n" . $number . "\nand never the twain shall meet." . mysqli_error($con));
+				die("Error: There's <em>your</em>\n" . $number . "\nand there's <em>my</em>\n" . $number . "\nand never the twain shall meet." . mysqli_error($con));
 				
 				return false;
 			}
 				else {
-					$addSample = "INSERT INTO Boxes (Number, Box, Row, Level) VALUES ('$number','$box','$roh','$level')";
+					$addSample = "INSERT INTO EMSampleBoxSorter (Number, Box, Row, Level) VALUES ('$number','$box','$roh','$level')";
 					if (!mysqli_query($con, $addSample)) {
 						die('Error: ' . mysqli_error($con));
 					}
--- viewSamples.php
+++ viewSamples.php
@@ -1,10 +1,10 @@
 <?php 
-	$con=mysqli_connect("localhost","root","passw0rd","cbiBoxSorter");
+	$con=mysqli_connect("localhost","root","","cbiTest");
 	//Check Connection
 	if (mysqli_connect_errno($con)) {
 		echo mysqli_connect_error() . "Connection Failed! It... came... from... behind!";
 	}
-	$query = "SELECT * FROM Boxes";
+	$query = "SELECT * FROM EMSampleBoxSorter";
 	$result = mysqli_query($con, $query) or die("no query");
 ?>
 <!doctype html>
@@ -20,9 +20,9 @@
 			text-align: center;
 		}
 		.col-sm-2 h4 {
-			font-weight: 300;
+			font-weight: 500;
 			font-size: 1.7em;
-			font-family: "Helvetica Neue", sans-serif;
+			font-family: "Raleway", sans-serif;
 		}
 		.row.sampleRow {
 			margin-bottom: 1%;
@@ -31,9 +31,9 @@
 			background-color: rgba(166, 166, 166, .75);
 			padding-top: 1%;
 			padding-bottom: 1%;
-			font-weight: 300;
+			font-weight: 400;
 			font-size: 1.3em;
-			font-family: "Helvetica Neue", sans-serif;
+			font-family: "Raleway", sans-serif;
 		}
 		.push-right {
 			margin-left: 5%;
@@ -82,15 +82,16 @@
 		</div>
 		<?php
 			while ($row = mysqli_fetch_assoc($result)) {
-				echo "<div class='row sampleRow'><div class='col-sm-2 col-sm-offset-1'>" . $row['Number'] . "\nhello\n</div> <div class='col-sm-2 push-right'>" . $row['Box'] . "</div> <div class='col-sm-2 push-right'>" . $row['Row'] . "</div> <div class='col-sm-2 push-right'>" . $row['Level'] . "</div></div>";
+				echo "<div class='row sampleRow'><div class='col-sm-2 col-sm-offset-1'>" . $row['Number'] . "</div> <div class='col-sm-2 push-right'>" . $row['Box'] . "</div> <div class='col-sm-2 push-right'>" . $row['Row'] . "</div> <div class='col-sm-2 push-right'>" . $row['Level'] . "</div></div>";
 			}
 		
 			mysqli_close($con);
 		?>
 
 	</div>
+<!-- jQuery -->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-
+<!-- bootstrap -->
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
